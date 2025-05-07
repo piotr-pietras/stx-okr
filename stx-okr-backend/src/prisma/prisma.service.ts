291b15fd064daf472bf54aee307c74f5fff333a4
Piotr <pietraspiotr94@gmail.com>
2025-05-07 11:57:04 +0200

    fix docker conf

--- stx-okr-backend/src/prisma/prisma.service.ts
+++ stx-okr-backend/src/prisma/prisma.service.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url:
+            process.env.DATABASE_URL ||
+            'postgresql://postgres:postgres@stx-okr-postgres:5432/postgres?schema=public',
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
